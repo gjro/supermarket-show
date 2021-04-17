5cf1db775ec0b167eece6ca3dc35173e3712b5c4
gjro <gilbertojr.ro@gmail.com>
2021-04-17 02:20:27 -0300

    First commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,11 @@
+const navBar = () => {
+    return ( 
+        <nav className="navBar">
+            <img src="" alt="" srcset=""/>
+            <a href="http://">Home</a>
+            <a href="http://">Carrinho</a>
+        </nav>
+     );
+}
+ 
+export default navBar;
